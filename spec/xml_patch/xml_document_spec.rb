dfbf94699c4b5adaaf41c8c75af0919d8283a29a
Iain Beeston <iainbeeston@users.noreply.github.com>
2017-11-03 06:57:20 +0000

    Refactored so that XmlDocument is the only class that knows about Oga
    The idea of XmlDocument was that it should encapsulate all xml access
    methods (so that we could change the xml gem used or provide
    adapters). However, DiffBuilder was also using oga, "leaking" the
    encapsulation of oga.
    
    To improve that I've refactored XmlDocument so that it provides a sax
    parsing interface, which is now used by DiffBuilder, and DiffBuilder
    no longer knows about the xml parsing library being used.
    
    Going forwards all xml handling should be performed by the XmlDocument
    class.

--- spec/xml_patch/xml_document_spec.rb
+++ spec/xml_patch/xml_document_spec.rb
@@ -4,18 +4,16 @@ require 'xml_patch/errors/invalid_xml'
 require 'xml_patch/errors/invalid_xpath'
 
 RSpec.describe XmlPatch::XmlDocument do
-  describe 'new' do
-    it 'raises an error if not given valid xml' do
-      expect {
-        described_class.new('<x:y:z></z>')
-      }.to raise_error(XmlPatch::Errors::InvalidXml)
-    end
-  end
-
   describe 'to_xml' do
     it 'is the current state of the document' do
       doc = described_class.new('<foo><bar /></foo>')
       expect(doc.to_xml).to eq('<foo><bar /></foo>')
+    end
+
+    it 'raises an error if not given valid xml' do
+      expect {
+        described_class.new('<x:y:z></z>').to_xml
+      }.to raise_error(XmlPatch::Errors::InvalidXml)
     end
   end
 
@@ -93,6 +91,22 @@ RSpec.describe XmlPatch::XmlDocument do
           doc.remove_at!('//////////')
         }.to raise_error(XmlPatch::Errors::InvalidXpath)
       end
+    end
+  end
+
+  describe 'parse' do
+    it 'yields once for every xml node in the document' do
+      doc = described_class.new('<foo x="1" y="2"><bar z="A"/></foo><baz />')
+      expect { |b|
+        doc.parse(&b)
+      }.to yield_successive_args(['foo', { 'x' => '1', 'y' => '2' }],
+                                 ['bar', { 'z' => 'A' }],
+                                 ['baz', {}])
+    end
+
+    it 'returns nil' do
+      doc = described_class.new('<foo />')
+      expect(doc.parse {}).to eq(nil)
     end
   end
 end
